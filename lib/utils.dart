e68959b79338d5a171058944d024c8b5d6408a18
ahmdtrdi <ahmadtriadi265@gmail.com>
2024-05-16 17:37:04 +0800

    feat: initial setup

---
+++ lib/utils.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+// **HEADINGS
+
+TextStyle mobileH1 = GoogleFonts.raleway(
+  fontSize: 40,
+  fontWeight: FontWeight.w700,
+);
+
+TextStyle mobileH2 = GoogleFonts.raleway(
+  fontSize: 32,
+  fontWeight: FontWeight.w700,
+);
+
+TextStyle mobileH3 = GoogleFonts.raleway(
+  fontSize: 26,
+  fontWeight: FontWeight.w700,
+);
+
+TextStyle mobileH4 = GoogleFonts.raleway(
+  fontSize: 24,
+  fontWeight: FontWeight.w700,
+);
+
+TextStyle mobileH5 = GoogleFonts.raleway(
+  fontSize: 22,
+  fontWeight: FontWeight.w700,
+);
+
+// ** BODY TEXT
+TextStyle bodyText20 = GoogleFonts.raleway(
+  fontSize: 20,
+);
+
+TextStyle bodyText17 = GoogleFonts.raleway(
+  fontSize: 17,
+);
+
+TextStyle bodyText16 = GoogleFonts.raleway(
+  fontSize: 16,
+);
+
+TextStyle bodyText15 = GoogleFonts.raleway(
+  fontSize: 15,
+);
+
+TextStyle bodyText14 = GoogleFonts.raleway(
+  fontSize: 14,
+);
+
+TextStyle bodyText12 = GoogleFonts.raleway(
+  fontSize: 12,
+);
+
+TextStyle bodyText10 = GoogleFonts.raleway(
+  fontSize: 10,
+);
+
+//color style
+
+// ** Neutral COLOR
+const neutral200 = Color(0XFFD9D9D9);
+const neutral600 = Color(0XFF82799D);
+
+// ** Turbo COLOR
+const Color turbo50 = Color(0XFFFFFFE7);
+const Color turbo100 = Color(0XFFFBFFC1);
+const Color turbo300 = Color(0XFFFFF394);
+const Color turbo600 = Color(0XFFCFAA00);
+const Color turbo800 = Color(0XFF885F0B);
+const Color turbo950 = Color(0XFF442904);
